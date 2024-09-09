d85df5297a4bea017024de37b9f3da031dc5631c
chetanpatle17 <chetanpatle17p@gmail.com>
2024-09-09 17:09:09 +0530

    stripe payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -53,8 +53,25 @@ const placeOrder = async (req,res)=>{
     }
 }
 
-//user orders for frontend
+const verifyOrder = async (req,res)=>{
+    const{orderId,success}=req.body;
+    try{
+        if(success=="true"){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true,message:"Paid"})
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false,message:"Not Paid"})
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
 
+//user orders for frontend
 const userOrders = async(req,res)=>{
     try {
         const orders = await orderModel.find({userId:req.body.userId});
@@ -65,4 +82,15 @@ const userOrders = async(req,res)=>{
     }
 }
 
-export {placeOrder,userOrders}
+//listing orders for admin panel
+const listOrders = async(req,res)=>{
+    try {
+        const orders = await orderModel.findOneAndDelete({});
+        res.json({success: true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders}
